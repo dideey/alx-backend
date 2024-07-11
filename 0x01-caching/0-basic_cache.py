2d2c39951bc2b0470b9d2f3be27a02357857c2e1
dideey <bradleyjd32@gmail.com>
2024-07-11 21:46:18 +0300

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
 """A method that impliments a basic key value chacing system
 """
-BaseCaching = __import__('base_chacing').BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ class that impliments a basic key value chacing system
+    """
+    a basic class for caching
     """
 
-    def __init__(self):
-        """calls the parent class constructor
-        """
-        super().__init__()
-
     def put(self, key, item):
-        """adds a key value pair to the chacing system
         """
-        if key or item is None:
-            return
-        self.cache_data[key] = item
+        Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """retrieves a value from the chacing system
         """
-        if key is None or key not in self.cache_data:
-            return None
+        Get an item by key
+        """
         return self.cache_data.get(key)
